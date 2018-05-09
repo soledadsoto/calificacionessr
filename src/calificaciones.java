0c9e7fdf754bb04f2b40e004676400e0f7a69e4f
soledadsoto <fpsoto2@gmail.com>
2018-05-09 16:36:17 +0200

    Añadimos comentarios JavaDoc al método veredicto.

--- src/calificaciones.java
+++ src/calificaciones.java
@@ -1,5 +1,13 @@
 public class calificaciones {
 
+
+    /**
+     *
+     * @param valor1 : esta cadena contendrá el argumento de args[3]
+     * @param valor2 : esta cadena contendrá el argumento de args[4]
+     * @return : nos devolverá un resultado que compara si valor1 es igual a valor2,
+     * o si valor1 es menor que valor2 o si valor1 es mayor que valor2.
+     */
     private static String veredicto(String valor1, String valor2){
         if (valor1.equals(valor2)){
             return "Perfecto. Has tenido buen criterio. ";
